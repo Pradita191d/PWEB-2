f89e7ae6e74f166bd41f5b9643bf097ca1477b29
Pradita191d <praditaruri17@gmail.com>
2023-10-16 21:04:21 +0700

    Update functions.php

--- jobsheet1/functions.php
+++ jobsheet1/functions.php
@@ -6,41 +6,41 @@
     <title>Functions</title>
 </head>
 <body>
-    <h4>Menghitung Luas dengan Input Statis <br></h4>
+    <h4>Menghitung Luas dengan Input Statis</h4>
     <?php
     function persegi_pjg($p, $l){
         return $p * $l;
     }
     echo "Luas Persegi Panjang = " . persegi_pjg(4, 5) . "<br>";
-    
+
     function lingkaran($jari_jari){
         return 3.14 * $jari_jari * $jari_jari;
     }
     echo "Luas Lingkaran = " . lingkaran(10) . "<br><br>";
     ?>
 
-    <h4>Menghitung Luas dengan Input Statis <br></h4>
-    <?php
-    ?>
+    <h4>Menghitung Luas dengan Input Dinamis</h4>
     <form method="POST">
-        Masukkan Jari-jari Lingkaran : <input type="number" name="r"></br>
-        Masukkan Panjang Persegi : <input type="number" name="p"></br>
-        Masukkan Lebar Persegi : <input type="number" name="l"></br>
+        Masukkan Jari-jari Lingkaran: <input type="number" name="r"><br>
+        Masukkan Panjang Persegi: <input type="number" name="p"><br>
+        Masukkan Lebar Persegi: <input type="number" name="l"><br>
         <input type="submit" value="Hitung">
-    </form>   
-    <?php
-    if (isset($_POST["r"])) {
-        $jari_jari = $_POST["r"];
-    }
-    echo "Luas Lingkaran = " . lingkaran($jari_jari);
-    ?>
-    <?php
-    if (isset($_POST["p"]) && isset($_POST["l"])) {
-        $p = $_POST["p"];
-        $l = $_POST["l"];
-    }
-    echo "Luas Persegi Panjang = " . persegi_pjg($p, $l);
-    ?>
+    </form>
 
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        if (isset($_POST["r"])) {
+            $jari_jari = $_POST["r"];
+            echo "Luas Lingkaran = " . lingkaran($jari_jari) . "<br>";
+        }
+        
+        
+        if (isset($_POST["p"]) && isset($_POST["l"])) {
+            $p = $_POST["p"];
+            $l = $_POST["l"];
+            echo "Luas Persegi Panjang = " . persegi_pjg($p, $l) . "<br>";
+        }        
+    }
+    ?>
 </body>
 </html>
