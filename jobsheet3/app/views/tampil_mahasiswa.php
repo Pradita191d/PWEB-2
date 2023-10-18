a2734c762fc7dae9fa9fb66626c6fd65c1167114
Pradita191d <praditaruri17@gmail.com>
2023-10-18 07:55:18 +0700

    Update tampil_mahasiswa.php

--- jobsheet3/app/views/tampil_mahasiswa.php
+++ jobsheet3/app/views/tampil_mahasiswa.php
@@ -32,8 +32,6 @@ $db = new database();
   </div>
 </nav>
 
-
-
 <h4>Data Mahasiswa</h4>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 <div class="mb-3">
